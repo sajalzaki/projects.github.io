1af8411d6641be86d8e0810421524d0155b48161
sajalzaki <sajal321@hotmail.com>
2016-01-17 00:10:48 -0500

    Update Tower-of-hanoi-recursive.cpp

--- Tower-of-hanoi-recursive.cpp
+++ Tower-of-hanoi-recursive.cpp
@@ -1,1 +1,34 @@
 
+/*
+* Problem: Write an iterative program
+* to solve the Towers of Hanoi problem.
+*/
+#include <iostream>
+using namespace std;
+// Hanoi Tower recursive Solution
+int count=0;
+void move(char from, char to, char mid, int n)
+{
+	
+	if(n==1)
+		cout<<"Move No:  " << ++count<<"  Ring number:"<<n<<" move from " << from << " to " << to << endl;
+	else
+	{
+		move(from, mid, to, n-1);
+		cout<<"Move No:  " << ++count<<"  Ring number:"<<n<<" move from " << from << " to " << to << endl;
+		move(mid, to, from, n-1);
+	}
+
+}
+int main()
+{
+	char from = '1';
+	char to = '3';
+	char mid = '2';
+	int n;
+	move(from, to , mid , 5);
+	
+	system("PAUSE");
+	return 0;
+
+}
